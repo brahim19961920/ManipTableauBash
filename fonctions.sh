b234d1a156ccfaaa2fbcadda784e4420f71dde81
brahim19961920 <brahimbenamiraca@gmail.com>
2021-01-13 22:17:29 +0100

    Ajout de la fonction moyenne et retouches sur les fonctions minimum et maximum

--- fonctions.sh
+++ fonctions.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-#A rajouter vérification du type des entrées+ fonction de tri +min+ max+moyenne
-
 #fonction pour remplir un tableau d'entiers
 Remplir_tableau(){
 
@@ -33,6 +31,12 @@ done
 #Fonction retournant le minimum d'un tableau d'entiers
 Minimum_tableau(){
 
+#on vérifie si le tableau n'est pas vide
+#{#tab[@]} est la taille du tanleau
+if test ${#tab[@]} -eq 0
+then
+	echo tableau vide
+else
 minimum=${tab[0]}
 for element in ${tab[*]}
 do
@@ -41,13 +45,22 @@ do
 		let minimum=$element
 	fi
 done
-echo $minimum
+
+echo "Le minimum du tableau est $minimum"
+fi
 }
 
 #Fonction retournant le maximum d'un tableau d'entiers
 Maximum_tableau(){
 
+#Si le tableau est vide
+if test ${#tab[@]} -eq 0
+then
+	echo tableau vide
+else
+#sinon, on cherche le maximum
 maximum=${tab[0]}
+
 for element in ${tab[*]}
 do
 	if test $maximum -lt $element
@@ -55,14 +68,43 @@ do
 		let maximum=$element
 	fi
 done
-echo $maximum
+echo "Le maximum du tableau est $maximum"
+fi
 }
-#Appel des fonctions
-Remplir_tableau
-Afficher_tableau
-echo "Le minimum de ce tableau est :$(Minimum_tableau)"
-echo "Le maximum de ce tableau est :$(Maximum_tableau)"
 
+#Fonction retournant la moyenne des élements du tableau
+Moyenne_tableau()
+{
+somme=0
+nb_elements=${#tab[@]}
+
+#On vérifie que le tableau n'est pas vide
+if test $nb_elements -eq 0
+then
+	echo "tableau vide"
+else
+	for element in ${tab[*]}
+	do
+		let somme=$somme+element
+	done
+
+	#on utilise | bc -l pour faire une division réelle
+	echo "La moyenne des éléments du tableau est $(expr "$somme/$nb_elements" | bc -l)"
+
+fi
+}
+
+#Appel des fonctions
+
+Remplir_tableau
+
+Afficher_tableau
+
+Minimum_tableau
+
+Maximum_tableau
+
+Moyenne_tableau
 
 
 
